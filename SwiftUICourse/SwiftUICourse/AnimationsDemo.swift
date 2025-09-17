b6533a99c19c56c836a9994f4ebfc33053b1b1c4
qre100g <kmnrddy4334@gmail.com>
2025-09-17 22:30:05 +0530

    Add animation for the gesture values

--- SwiftUICourse/SwiftUICourse/AnimationsDemo.swift
+++ SwiftUICourse/SwiftUICourse/AnimationsDemo.swift
@@ -16,6 +16,7 @@ struct AnimationsDemo: View {
                 NavigationLink("Animating Bindings") { AnimatingBindings() }
                 NavigationLink("Explict Animation") { ExplicitAnimation() }
                 NavigationLink("Animation Stack") { AnimationStack() }
+                NavigationLink("Animating Gesture") { AnimatingGesture() }
                 
             }
             .navigationTitle("Animations")
@@ -115,6 +116,35 @@ struct AnimationStack: View {
     }
 }
 
+struct AnimatingGesture: View {
+    private let letters = Array("Hello, SwiftUI!")
+    @State private var dragAmount: CGSize = .zero
+    @State private var enabled: Bool = false
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count, id: \.self) { num in
+                Text(String(self.letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(.linear.delay(Double(num) / 20), value: dragAmount)
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged {
+                    dragAmount = $0.translation
+                }
+                .onEnded { _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
+        )
+    }
+}
+
 #Preview {
-    AnimationsDemo()
+    AnimatingGesture()
 }
