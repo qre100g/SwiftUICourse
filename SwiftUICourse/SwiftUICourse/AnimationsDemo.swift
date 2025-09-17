de3df3e79adc98ade170a68183eabe077060a2ea
qre100g <kmnrddy4334@gmail.com>
2025-09-17 23:48:53 +0530

    Add custom transtions demo view.

--- SwiftUICourse/SwiftUICourse/AnimationsDemo.swift
+++ SwiftUICourse/SwiftUICourse/AnimationsDemo.swift
@@ -17,6 +17,8 @@ struct AnimationsDemo: View {
                 NavigationLink("Explict Animation") { ExplicitAnimation() }
                 NavigationLink("Animation Stack") { AnimationStack() }
                 NavigationLink("Animating Gesture") { AnimatingGesture() }
+                NavigationLink("View transition demo") { ViewTransitionDemo() }
+                NavigationLink("Custom transitions") { CustomTransitionDemo() }
                 
             }
             .navigationTitle("Animations")
@@ -165,6 +167,47 @@ struct ViewTransitionDemo: View {
     }
 }
 
+struct CustomTransitionDemo: View {
+    @State private var isShowingRed = false
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
+            }
+        }
+        .onTapGesture {
+            withAnimation {
+                isShowingRed.toggle()
+            }
+        }
+    }
+}
+
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+    }
+}
+
 #Preview {
-    ViewTransitionDemo()
+    AnimationsDemo()
 }
