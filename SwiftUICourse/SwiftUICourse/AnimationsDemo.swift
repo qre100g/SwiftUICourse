22af02348bd33ba2f79d3ba0ff2e99ea58147e8c
qre100g <kmnrddy4334@gmail.com>
2025-09-17 22:40:59 +0530

    Add a transition animation for the view.

--- SwiftUICourse/SwiftUICourse/AnimationsDemo.swift
+++ SwiftUICourse/SwiftUICourse/AnimationsDemo.swift
@@ -145,6 +145,26 @@ struct AnimatingGesture: View {
     }
 }
 
+struct ViewTransitionDemo: View {
+    @State private var isShowingRed = false
+    var body: some View {
+        VStack {
+            Button("Tap me") {
+                withAnimation {
+                    isShowingRed.toggle()
+                }
+            }
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+            }
+        }
+    }
+}
+
 #Preview {
-    AnimatingGesture()
+    ViewTransitionDemo()
 }
